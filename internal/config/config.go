daa2f839ba35002701fbb1524578b0e68314d27d
arthur <ridpfrep@yandex.ru>
2025-09-26 20:49:22 +0300

    del file

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ import (
 type Config struct {
 	ServerAddress string `json:"server_address" env:"SERVER_ADDRESS"`
 	BaseURL       string `json:"base_url" env:"BASE_URL"`
-	FilePath        string `json:"file_path" env:"FILE_PATH"`
+	FilePath      string `json:"file_path" env:"FILE_PATH"`
 }
 
 func LoadConfig() *Config {
@@ -17,7 +17,7 @@ func LoadConfig() *Config {
 
 	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for short links")
-	flag.StringVar(&cfg.FilePath, "f", "/home/art/code/goshan/good.json", "File to restore DB")
+	flag.StringVar(&cfg.FilePath, "f", "", "File to restore DB")
 	flag.Parse()
 	// Приоритет у .env перемененных, т.е. если выше нашлись с флагом - ниже строка перезатрет их
 	env.Parse(&cfg)
