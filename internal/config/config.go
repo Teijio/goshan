f412ab9b1c2171bf0eaedc63ce6d296b5007f349
arthur <ridpfrep@yandex.ru>
2025-09-26 20:31:07 +0300

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,7 +17,7 @@ func LoadConfig() *Config {
 
 	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for short links")
-	flag.StringVar(&cfg.FilePath, "f", "/home/art/code/goshan/good.json", "File to restore DB")
+	flag.StringVar(&cfg.FilePath, "f", "", "File to restore DB")
 	flag.Parse()
 	// Приоритет у .env перемененных, т.е. если выше нашлись с флагом - ниже строка перезатрет их
 	env.Parse(&cfg)
