c154f69af2e2b88bcde3339b63bcbafbb07a0020
arthur <ridpfrep@yandex.ru>
2025-10-31 19:43:22 +0300

    goro added

--- internal/models/models.go
+++ internal/models/models.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/Teijio/goshan/internal/config"
 )
 
 type ShortURL struct {
-	OriginalURL   string `json:"url"`
-	ID            string `json:"id"`
-	CreatedByID   string `json:"created_by"`
-	CorrelationID string `json:"correlation_id"`
+	OriginalURL   string     `db:"original_url" json:"url"`
+	ID            string     `db:"id" json:"id"`
+	CreatedByID   string     `db:"created_by" json:"created_by"`
+	CorrelationID string     `db:"correlation_id" json:"correlation_id"`
+	IsDeleted     *time.Time `db:"is_deleted" json:"is_deleted"` // nil = не удалено, значение = время удаления
 
 	Cfg *config.Config
 }
